726f45b6d1a461f3d15bf516a62152dae2033a03
Danilson Kayumbuca <artmiss264@gmail.com>
2023-04-05 22:24:26 +0100

    +navbar, componentes no app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Works, Tech, StarsCanvas, } from './components';
 
 const App = () => {
 
   return (
-
-    <div>
-
-      <h1>3D Porfolio</h1>
-
-    </div>
-
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+const [Active, setActive] = useState('');
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <nav className={`${styles.paddingX} w-full flex items-center py- fixed top-0 z-20`}>
+        <div className='w-full flex justify-center items-center max-w-7xl mx-auto'>
+
+          <Link
+            to='/'
+            className='flex items-center gap-2'
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0)
+            }}
+          >
+
+            <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          </Link>
+
+        </div>
+      </nav>
+    </div>
   )
 }
 
